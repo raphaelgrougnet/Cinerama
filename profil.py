bb52e06943ddde52765b6df7634029e682b088c8
Julien Paquet-Beaudoin <paqj8978@gmail.com>
2023-11-15 13:52:51 -0500

    Consulter parties profils
    Affichage des données sur les pages profils

--- profil.py
+++ profil.py
@@ -1,4 +1,9 @@
-from flask import redirect, render_template, request, session, Blueprint, abort, current_app as app
+import os
+import re
+import dotenv
+from flask import Flask, redirect, render_template, request, session, Blueprint, abort, current_app as app
+from bd import mongo
+from bson.objectid import ObjectId
 
 bp_profil = Blueprint('profil', __name__)
 
@@ -10,10 +15,31 @@ def profil():
     Affiche le profil de l'utilisateur
     """
     utilisateur = session.get("utilisateur")
-
+    
     if utilisateur is None:
         abort(404)
-    return render_template('profil/profil.html', utilisateur=utilisateur)
+    
+    user = mongo.db.users.find_one(
+        {
+            "_id": ObjectId(utilisateur['_id'])
+        }
+    )
+    
+    commentaires = list(mongo.db.commentaires.find(
+        {
+            "id_user": ObjectId(user['_id'])
+        }
+    ))
+
+    for c in commentaires:
+        film = mongo.db.films.find_one({"_id": ObjectId(c['id_film'])})
+        c['nom_film'] = film['Title']
+        date = c["date_post"]
+        c["date_post"] = date.strftime("%d-%m-%Y")
+
+        
+    
+    return render_template('profil/profil.html', utilisateur=utilisateur, commentaires=commentaires)
 
 @bp_profil.route('/favoris')
 def favoris():
@@ -24,7 +50,16 @@ def favoris():
 
     if utilisateur is None:
         abort(404)
-    return render_template('profil/profil_favoris.html', utilisateur=utilisateur)
+    
+    user = mongo.db.users.find_one({"_id": ObjectId(utilisateur['_id'])})
+    id_film = user['favoris']
+    films = []
+    for id in id_film:
+        films.append(mongo.db.films.find_one({"_id": ObjectId(id)}))
+       
+   
+
+    return render_template('profil/profil_favoris.html', utilisateur=utilisateur, films=films)
 
 @bp_profil.route('/visionne')
 def visionne():
@@ -32,6 +67,20 @@ def visionne():
     Affiche les vidéos visionnées de l'utilisateur
     """
     utilisateur = session.get("utilisateur")
+    
     if utilisateur is None:
         abort(404)
-    return render_template('profil/profil_visionne.html', utilisateur=utilisateur)
+
+    user = mongo.db.users.find_one({"_id": ObjectId(utilisateur['_id'])})
+    visionnes = user['visionnes']
+
+    sorted_vis = sorted(visionnes, key=lambda x: x["date_visionne"], reverse=True)
+    films = []
+    for v in sorted_vis:
+        date = v["date_visionne"]
+        v["date_visionne"] = date.strftime("%d-%m-%Y")
+        film = mongo.db.films.find_one({"_id": ObjectId(v['id_film'])})
+        film['date_visionne'] = v["date_visionne"]
+        films.append(film)
+    
+    return render_template('profil/profil_visionne.html', utilisateur=utilisateur, films=films)
